1dd5bfaf705269604197cad1d0a575e8f5b0ba32
Salvatore Fasano <fasanosalvatore@hotmail.it>
2022-08-24 15:44:24 +0200

    build: last published version of package

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,11 @@ export * from './messages/events/project-result-event';
 export * from './messages/listeners/base-listener';
 
 export * from './messages/publishers/base-publisher';
+export * from './messages/publishers/project-ready-publisher';
+
+export * from './models/application';
+export * from './models/test-run';
+export * from './models/user';
 
 export * from './constants';
 export * from './utility';
